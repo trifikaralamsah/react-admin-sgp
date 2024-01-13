24ca28223c025ba91ddb1dc1b40d516524c1835c
trifikar <trifikar24@gmail.com>
2024-01-13 14:15:44 +0700

    chore(docker): add dockerfile dev and staging

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: 3030,
   },
   preview: {
-    port: 8080,
+    port: 7070,
   },
 });
